f2a69129b08fa6b551e7c24640053aa47752bcb4
Konyuka <michaelsaiba84@gmail.com>
2025-03-07 12:46:49 +0300

    deploy

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,10 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/', [PostController::class, 'home'])->name('home');
+// Route::get('/', [PostController::class, 'home'])->name('home');
+Route::get('/', function () {
+    return redirect('/admin/login');
+});
 Route::get('/search', [PostController::class, 'search'])->name('search');
 Route::get('/about-us', [SiteController::class, 'about'])->name('about-us');
 Route::get('/category/{category:slug}', [PostController::class, 'byCategory'])->name('by-category');
